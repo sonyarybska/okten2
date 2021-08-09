bb66bb35528d494f1efc650b96db07810d2db468
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-08-09 16:44:51 +0300

    new

---
+++ react/react-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Simpsons} from "./components/simpsons/Simpsons";
+
+function App() {
+  return (
+    <div className="App">
+   <Simpsons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-project/src/components/simpsons/Simpsons.js
@@ -0,0 +1,7 @@
+export function Simpsons(){
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
